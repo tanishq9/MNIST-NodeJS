5fb47286c369899f919bcae8c08e37b628d77013
Tanishq Saluja <tanishqsaluja18@gmail.com>
2019-04-09 04:47:47 +0530

    jbb

--- model.py
+++ model.py
@@ -6,21 +6,21 @@ from keras.preprocessing import image
 
 def predict():
     #print(sys.path)
-    print("Process started")
+    #print("Process started")
     new_model = load_model('mnist_model_keras_collab.h5')
-    print('model_loaded')
+    #print('model_loaded')
     img = image.load_img(path='input.png',color_mode="grayscale",target_size=(28,28,1))
-    print('image_loaded')
+    #print('image_loaded')
     img = image.img_to_array(img)
-    print('running_inference')
+    #print('running_inference')
     output = new_model.predict(img.reshape(1,28,28,1)) # To do it for a new data point
-    print('Got the result !')
+    #print('Got the result !')
     output.argmax()
     print(output.argmax())
 
 if __name__ == "__main__":
-    print(sys.version)
-    print(keras.__version__)
-    print(tensorflow.__version__)
-    print("Predict function called")
+    #print(sys.version)
+    #print(keras.__version__)
+    #print(tensorflow.__version__)
+    #print("Predict function called")
     predict()
